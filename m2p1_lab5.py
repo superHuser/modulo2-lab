79d859d49f775cafc92615d4a371b57bf30ca154
Gilson Nunes Junior <gnunesju@everis.com>
2019-05-09 21:56:54 -0300

    Corrigido | m2p1_lab5.py

--- m2p1_lab5.py
+++ m2p1_lab5.py
@@ -19,8 +19,10 @@ from  lib.scrapy_dadosAbertos import DadosAbertos
 # Instanciamos objetos
 listJson = DadosAbertos()
 
+# Percorre os itens da lista
 for item in listJson.orgaos():
     id = item['id']
     nome = item['nome']
+    # O décimo ID é 2009, vamos imprimir até ele
     if int(id) <= 2009 :
         print(id, nome.upper())
