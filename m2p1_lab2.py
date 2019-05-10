20066ded6b3888eaf68261c7c0b861bc3d1116d6
Gilson Nunes Junior <gnunesju@everis.com>
2019-05-09 21:35:38 -0300

    Resolvido | m2p1_lab2.py

--- m2p1_lab2.py
+++ m2p1_lab2.py
@@ -10,30 +10,19 @@ Professor   : Clodonil Honorio Trigo
 Módulo      : https://github.com/clodonil/Python-Fundamentals/tree/master/modulo2/parte1/Labs/code
 
 Data        : 08 de Maio de 2019
-Descrição   : 
+Descrição   : Programa com otimização de execução; exibe cada ID de deputado, seu nome e sigla do partido.
 '''
 
+# Importamos a biblioteca
 from  lib.scrapy_dadosAbertos import DadosAbertos
 
-list_dep = DadosAbertos()
+# Instanciamos objetos
+listaDeputados = DadosAbertos()
+mascara = "Id: {0}\n> Nome: {1}\n> Sigla do Partido: {2}\n"
 
-print(len(list_dep.deputados()))
-
-
-for dep in list_dep.deputados():    
-    id   = dep['id']
-    nome = dep['nome']
-
-    infodep = list_dep.deputado_id(id)
-    
-    mascara = "Id: {0}\n Nome: {1}\n Data de Nascimento: {2}\n Estado de Nascimento: {3}\n Email: {4}\n Sigla do Partido: {5}\n #-------------------------------------------------"
-
-    print(mascara.format( 
-                         id,
-                         nome,
-                         infodep['dataNascimento'],
-                         infodep['ufNascimento'],
-                         infodep['ultimoStatus']['gabinete']['email'],
-                         infodep['ultimoStatus']['siglaPartido']
-                         ))
-print(list_dep.deputados())
+# Percorre cada item na lista
+for item in listaDeputados.deputados():
+    id = item['id']
+    nome = item['nome']
+    # Imprime utilizando a máscara
+    print(mascara.format(id, nome, item['siglaPartido']))
